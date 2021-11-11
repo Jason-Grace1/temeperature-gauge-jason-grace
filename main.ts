72940f516307a20c141a6e50a0ad86e447386863
Jason-Grace1 <92944114+Jason-Grace1@users.noreply.github.com>
2021-11-11 08:30:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+let temperature = 0
 basic.forever(function () {
-	
+    temperature = input.temperature()
+    if (temperature < 20) {
+        temperature = 20
+    } else if (temperature > 35) {
+        temperature = 35
+    }
+    pins.servoWritePin(AnalogPin.P0, pins.map(
+    temperature,
+    20,
+    35,
+    0,
+    180
+    ))
 })
